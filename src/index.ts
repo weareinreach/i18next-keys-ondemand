de1b1d3e03153daa74c8c8ef9ba54eceb146e7fa
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-03-01 19:57:04 -0500

    pass `defaultValue` through getter

--- src/index.ts
+++ src/index.ts
@@ -1,85 +1,100 @@
-/// <reference path="./i18next.d.ts" />
-import * as debounce from 'debounce';
+import { i18n } from 'i18next'
+import * as debounce from 'debounce'
 
 export interface TranslationMap {
-    [key: string]: string;
+	[key: string]: string
 }
 
-export type TranslationGetter = (keys: string[], language: string, namespace: string) => Promise<TranslationMap>;
+export type TranslationGetter = (
+	keys: string[],
+	language: string,
+	namespace: string,
+	defaultValues: Record<string, string>
+) => Promise<TranslationMap>
 
 interface KeyQueue {
-  [path: string]: KeysSet;
+	[path: string]: KeysSet
 }
 interface KeysSet {
-  [key: string]: boolean;
+	/** value passed as `defaultValue` */
+	[key: string]: string
 }
 
 export interface Options {
-    /**
-     * The resource key translator
-     * 
-     * @type {TranslationGetter}
-     * @memberof Options
-     */
-    translationGetter: TranslationGetter;
+	/**
+	 * The resource key translator
+	 *
+	 * @type {TranslationGetter}
+	 * @memberof Options
+	 */
+	translationGetter: TranslationGetter
 
-    /**
-     * Value to return for missing keys (default: empty string)
-     * 
-     * @type {string}
-     * @memberof Options
-     */
-    missingKeyValue?: string;
+	/**
+	 * Value to return for missing keys (default: empty string)
+	 *
+	 * @type {string}
+	 * @memberof Options
+	 */
+	missingKeyValue?: string
 
-    /**
-     * Delay in ms used to debounce the translation requests (default: 100ms)
-     * 
-     * @type {number}
-     * @memberof Options
-     */
-    debounceDelay?: number;
+	/**
+	 * Delay in ms used to debounce the translation requests (default: 100ms)
+	 *
+	 * @type {number}
+	 * @memberof Options
+	 */
+	debounceDelay?: number
 }
 
 export class I18nextKeysOnDemand {
+	type = '3rdParty'
+	options: Options
 
-    type = '3rdParty';
-    options: Options;
+	constructor(options: Options) {
+		this.options = { debounceDelay: 100, missingKeyValue: '', ...options }
+	}
 
-    constructor(options: Options) {
-        this.options = { debounceDelay: 100, missingKeyValue: '', ...options };
-    }
+	public init(instance: i18n) {
+		const missingKeysQueue: KeyQueue = {}
+		const options = this.options
 
-    public init(instance: i18next.i18n) {
+		function requestResources(lng: string, ns: string) {
+			const path = `${lng}.${ns}`
+			options
+				.translationGetter(
+					Object.keys(missingKeysQueue[path]),
+					lng,
+					ns,
+					missingKeysQueue[path]
+				)
+				.then((result) => {
+					missingKeysQueue[path] = {}
+					instance.addResources(lng, ns, result)
+				})
+		}
 
-        const missingKeysQueue: KeyQueue = {};
-        const options = this.options;
+		const debouncedRequestResources: { [path: string]: () => void } = {}
+		function requestKey(key: string, lng: string, ns: string, res: string) {
+			const path = `${lng}.${ns}`
+			missingKeysQueue[path] = missingKeysQueue[path] || {}
+			missingKeysQueue[path][key] = res
 
-        function requestResources(lng: string, ns: string) {
-            const path = `${lng}.${ns}`;
-            options.translationGetter(Object.keys(missingKeysQueue[path]), lng, ns).then((result) => {
-                missingKeysQueue[path] = {};
-                instance.addResources(lng, ns, result);
-            });
-        }
-    
-        const debouncedRequestResources: {[path: string]: () => void } = {};
-        function requestKey(key: string, lng: string, ns: string) {
-        const path = `${lng}.${ns}`;
-        missingKeysQueue[path] = missingKeysQueue[path] || {};
-        missingKeysQueue[path][key] = true;
-            
-        debouncedRequestResources[path] = debouncedRequestResources[path] ||
-                                            debounce(() => requestResources(lng, ns), options.debounceDelay);
-        debouncedRequestResources[path]();
-        }
-    
-        instance.on('missingKey', (lngs: string | string[], ns: string, key: string, res: string) => {
-        instance.options.parseMissingKeyHandler = () => {
-            return options.missingKeyValue;
-        };
+			debouncedRequestResources[path] =
+				debouncedRequestResources[path] ||
+				debounce(() => requestResources(lng, ns), options.debounceDelay)
+			debouncedRequestResources[path]()
+		}
 
-        const languages = typeof lngs === 'string' ? [ lngs ] : lngs;
-        languages.map(l => requestKey(key, l, ns));
-        });
-    }
+		instance.on(
+			'missingKey',
+			(lngs: string | string[], ns: string, key: string, res: string) => {
+				instance.options.parseMissingKeyHandler = () => {
+					return options.missingKeyValue
+				}
+
+				const languages = typeof lngs === 'string' ? [lngs] : lngs
+				languages.map((l) => requestKey(key, l, ns, res))
+			}
+		)
+	}
 }
